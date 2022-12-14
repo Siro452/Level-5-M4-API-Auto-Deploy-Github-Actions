be33ca46f77b0d7ddc8ac90ccddc60c23a1ecb1e
Nick Yeung <n.yeung91@gmail.com>
2022-12-14 23:05:38 +1300

    test

--- sumChars.js
+++ sumChars.js
@@ -2,25 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 //console log parameters for function
 function sumChars(carModel, carYear) {
-    // filter carmodel to only get letters
-    let myNumber = carModel.replace(/[0-9\s]/g, "");
-    //loops through every letter and assigns each letter with a number
-    let i, n = myNumber.length, acc = 0;
-    for (i = 0; i < n; i++) {
-        //adds each letter assigned number to each other, - 9 to subtract from radix 36 to = 26
-        acc += parseInt(myNumber[i], 36) - 9;
-    }
-    //add total of the loop with the second param of year
-    let totalSum = acc * 100 + Number(carYear);
-    if (totalSum < 0) {
-        return "number cannot be a negative";
-    }
-    else if (isNaN(totalSum)) {
-        return "invalid character";
-    }
-    else
-        return "$" + totalSum;
+  // filter carmodel to only get letters
+  let myNumber = carModel.replace(/[0-9\s]/g, "");
+  //loops through every letter and assigns each letter with a number
+  let i,
+    n = myNumber.length,
+    acc = 0;
+  for (i = 0; i < n; i++) {
+    //adds each letter assigned number to each other, - 9 to subtract from radix 36 to = 26
+    acc += parseInt(myNumber[i], 36) - 9;
+  }
+  //add total of the loop with the second param of year
+  let totalSum = acc * 100 + Number(carYear);
+  if (totalSum < 0) {
+    return "number cannot be a negative";
+  } else if (isNaN(totalSum)) {
+    return "invalid character";
+  } else return "$" + totalSum;
 }
-exports.default = sumChars;
+module.exports = sumChars;
 console.log(sumChars("civic", 3014));
 console.log(sumChars("911", 2932));
--- sumChars.spec.js
+++ sumChars.spec.js
@@ -1,39 +1,40 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const sumChars_1 = __importDefault(require("./sumChars")); // import car value function
-const globals_1 = require("@jest/globals");
+const sumChars = require("./sumChars.js"); // import car value function
+// import { describe, expect, it } from "@jest/globals";
 const errorNegative = "number cannot be a negative";
 const errorWrongChar = "invalid character";
-(0, globals_1.describe)("sum normal", () => {
-    (0, globals_1.it)("normal conditions and input", () => {
-        (0, globals_1.expect)((0, sumChars_1.default)("civic", 2014)).toBe("$6614");
-    });
+
+describe("sum normal", () => {
+  it("normal conditions and input", () => {
+    expect(sumChars("civic", 2014)).toBe("$6614");
+  });
 });
-(0, globals_1.describe)(" only numbers ", () => {
-    (0, globals_1.it)("Numbers only is ok", () => {
-        (0, globals_1.expect)((0, sumChars_1.default)("911", 2020)).toBe("$2020");
-    });
+
+describe(" only numbers ", () => {
+  it("Numbers only is ok", () => {
+    expect(sumChars("911", 2020)).toBe("$2020");
+  });
 });
-(0, globals_1.describe)("negative values", () => {
-    (0, globals_1.it)("Negative year gives wrong error", () => {
-        (0, globals_1.expect)((0, sumChars_1.default)("Task-Force", -987)).toBe(errorNegative);
-    });
+
+describe("negative values", () => {
+  it("Negative year gives wrong error", () => {
+    expect(sumChars("Task-Force", -987)).toBe(errorWrongChar);
+  });
 });
-(0, globals_1.describe)("values are wrong types", () => {
-    (0, globals_1.it)("Wrong data type gives wrong error", () => {
-        (0, globals_1.expect)((0, sumChars_1.default)("C200", "twenty twenty")).toBe(errorWrongChar);
-    });
+
+describe("values are wrong types", () => {
+  it("Wrong data type gives wrong error", () => {
+    expect(sumChars("C200", "twenty twenty")).toBe(errorWrongChar);
+  });
 });
-(0, globals_1.describe)("inverse value types in wrong positions", () => {
-    (0, globals_1.it)("if string and number were inverse values gives error", () => {
-        (0, globals_1.expect)((0, sumChars_1.default)("213123", "fatcar")).toBe(errorWrongChar);
-    });
+
+describe("inverse value types in wrong positions", () => {
+  it("if string and number were inverse values gives error", () => {
+    expect(sumChars("213123", "fatcar")).toBe(errorWrongChar);
+  });
 });
-(0, globals_1.describe)("special cahracters or spaces", () => {
-    (0, globals_1.it)("special character or string in wrong position will give error, spaces are ignored", () => {
-        (0, globals_1.expect)((0, sumChars_1.default)("ci vic^", "2014$%")).toBe(errorWrongChar);
-    });
+
+describe("special cahracters or spaces", () => {
+  it("special character or string in wrong position will give error, spaces are ignored", () => {
+    expect(sumChars("ci vic^", "2014$%)")).toBe(errorWrongChar);
+  });
 });
